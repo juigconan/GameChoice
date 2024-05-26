b1feadb7c8575c22ffd8d79b9cf85d2adab4fdb1
juigconan <juigconantrabajo@gmail.com>
2024-05-26 23:18:00 +0200

    Creado el fragmento home, parte de main y añadidos strings para la API

---
+++ app/src/main/java/com/example/gamechoice/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.gamechoice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gamechoice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
+    }
+    fun setListeners(){
+        binding.ibHome.setOnClickListener()
+    }
+}
